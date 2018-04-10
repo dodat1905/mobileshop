43c164d2a798ed1d0f595afc3069cc9b8b9e479c
DoDat <dodat@ubuntu.ubuntu-domain>
2018-04-10 14:22:09 +0700

    fix order part 2

--- app/models/support_coupon.rb
+++ app/models/support_coupon.rb
@@ -1,5 +1,5 @@
 class SupportCoupon
-  attr_reader :order
+  attr_reader :order, :coupon
 
   def initialize args = {}
     @order = args[:order]
@@ -9,7 +9,19 @@ class SupportCoupon
     @line_items ||= order.line_items
   end
 
-  def coupon
+  def find_coupon
     @coupon = Coupon.find_by code: order.coupon_code
+  end
+
+  def show_sale_price
+    order.line_items.inject(0) do |sum, price|
+      sum + price.sale_price(coupon)
+    end
+  end
+
+  def show_total_price
+    order.line_items.inject(0) do |sum, price|
+      sum + price.total_price
+    end
   end
 end
